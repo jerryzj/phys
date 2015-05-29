0b8cc43ed1865aff2e22408f31d49a2bfe34642e
Jerry ZJ <jerry0715no14@gmail.com>
2015-05-30 00:17:19 +0800

    phys34

---
+++ 普物34.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+void one();
+
+int main()
+{
+	int choose
+	
+	
+	cout<<"1.Find λ medium(μm) for Earth, considered a ?-K blackbody.">>endl; 
+	cout<<""
+	cout<<"請選擇題目">>endl;
+	cin>>choose;
+	switch(choose){
+		case 1:break;
+		case 2:break;
+		case 3:break;
+		case 4:
+	}
+
+	return 0;
+}
+
+void one{
+	int K;
+	cout<<"Temperature is(K)?"<<endl;
+	cin>>K;
+
+	cout<<(4.11/K)<<endl;
+}
